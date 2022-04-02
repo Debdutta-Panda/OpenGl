10337d665ff4883e32690c349448393a5fd0bf10
Debdutta Panda <bedmad82@gmail.com>
2022-04-03 00:05:38 +0530

    environment setup

--- app/src/main/java/com/debduttapanda/opengl/MainActivity.kt
+++ app/src/main/java/com/debduttapanda/opengl/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.debduttapanda.opengl
 
+import android.app.Activity
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : Activity() {
+    private lateinit var gLView: GLSurfaceView
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
     }
 }
